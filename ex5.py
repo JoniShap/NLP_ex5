e005c55cc20662c8351e1152f6c129819f944e76
yonashap <jonathan.shapira@mail.huji.ac.il>
2025-01-21 16:12:07 +0200

    g

--- ex5.py
+++ ex5.py
@@ -4,3 +4,21 @@ page = wikipedia.page('Bradley Pitt').content
 analyzed_page = nlp(page)
 
 
+def find_proper_noun_sequences(doc):
+    proper_nouns = []
+    current_sequence = []
+
+    for token in doc:
+        if token.pos_ == "PROPN":
+            current_sequence.append(token)
+        else:
+            if current_sequence:  # If we have a sequence
+                if len(current_sequence) > 0:
+                    proper_nouns.append(current_sequence)
+                current_sequence = []
+
+    # Don't forget the last sequence
+    if current_sequence:
+        proper_nouns.append(current_sequence)
+
+    return proper_nouns
